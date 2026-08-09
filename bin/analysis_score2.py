97a367a75769e5f150e00b4db541e3dc405bcc64
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2026-08-09 12:46:04 +0100

    More profiling

--- bin/analysis_score2.py
+++ bin/analysis_score2.py
@@ -21,7 +21,8 @@ def main():
     _n_male = 121333
     n_female_over_3 = 54227
     n_male_over_3 = 40444
-    dry_run = False
+    #dry_run = False
+    dry_run = True
 
     # set up parameters
     if dry_run:
